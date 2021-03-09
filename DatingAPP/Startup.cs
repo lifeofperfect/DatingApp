3b10d4ac97b41a43d959f208d20eb99e2dd5c251
perfect igbadumhe <perfect.igbadumhe@gmail.com>
2021-03-09 19:38:52 +0100

    add angular

--- DatingAPP/Startup.cs
+++ DatingAPP/Startup.cs
@@ -35,6 +35,7 @@ namespace DatingAPP
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DatingAPP", Version = "v1" });
             });
             services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +51,8 @@ namespace DatingAPP
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
