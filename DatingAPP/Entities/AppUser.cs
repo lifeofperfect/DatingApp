7f15b1b59aeedebadc24d44464323c4200b665de
Perfect Imhetsike Igbadumhe <PerfectImhetsike.Igbadumhe@stanbicibtc.com>
2021-04-05 17:29:41 +0100

    added password to database and migrate to db

--- DatingAPP/Entities/AppUser.cs
+++ DatingAPP/Entities/AppUser.cs
@@ -10,5 +10,7 @@ namespace DatingAPP.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
